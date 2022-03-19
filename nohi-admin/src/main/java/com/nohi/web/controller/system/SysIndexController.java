e27b6c507f8419ba3685bd77500d85a205847790
dute7liang <383200134@qq.com>
2022-03-20 00:34:27 +0800

    feature: 更新默认的账号密码名字和代码生成

--- nohi-admin/src/main/java/com/nohi/web/controller/system/SysIndexController.java
+++ nohi-admin/src/main/java/com/nohi/web/controller/system/SysIndexController.java
@@ -17,13 +17,13 @@ public class SysIndexController {
      * 系统基础配置
      */
     @Autowired
-    private NoHiConfig ruoyiConfig;
+    private NoHiConfig nohiConfig;
 
     /**
      * 访问首页，提示语
      */
     @RequestMapping("/")
     public String index() {
-        return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", ruoyiConfig.getName(), ruoyiConfig.getVersion());
+        return StringUtils.format("欢迎使用{}后台管理框架，当前版本：v{}，请通过前端地址访问。", nohiConfig.getName(), nohiConfig.getVersion());
     }
 }
